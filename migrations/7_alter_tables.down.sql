1397102abdcf8039e6141d2c05196ca224f56dc5
shahabrat <shahbaxtiyorov2@gmail.com>
2021-09-27 11:12:58 +0500

    edited migrations of respondent

---
+++ migrations/7_alter_tables.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE respondent
+    ALTER COLUMN rating_communication DROP default,
+    ALTER COLUMN rating_experience DROP default,
+    ALTER COLUMN rating_punctuality DROP default;
+    
---
+++ migrations/7_alter_tables.up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE respondent
+    ALTER COLUMN rating_communication SET default 0,
+    ALTER COLUMN rating_experience SET default 0,
+    ALTER COLUMN rating_punctuality SET default 0;
+    
